d1316954471e98b7e29b2cccb577beba00d03078
ZviRosenfeld <zviRosenfeld2@gmail.com>
2021-04-13 08:59:44 +0300

    Removed extra using

--- IEnumerableCorrelater.UnitTests/Correlaters/LcsCorrelatersAreTheSameTests.cs
+++ IEnumerableCorrelater.UnitTests/Correlaters/LcsCorrelatersAreTheSameTests.cs
@@ -1,7 +1,6 @@
 ﻿using IEnumerableCorrelater.Correlaters;
 using IEnumerableCorrelater.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
